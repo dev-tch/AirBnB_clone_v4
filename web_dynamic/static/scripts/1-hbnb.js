2c7ef6aa11fac7742012a7bf3620bb84bd038c56
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-11 14:39:03 +0000

    fix task 1 web dynamic

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,31 @@
-$(document).ready(function() {
-    const checkedAmenities = {};
-    $('input[type="checkbox"]').change(function() {
-        const amenityId = $(this).attr('data-id');
-        const amenityName = $(this).attr('data-name');
-        if ($(this).is(':checked')) {
-            checkedAmenities[amenityId] = amenityName;
-        } else {
-             delete checkedAmenities[amenityId];
-        }
-        const amenitiesList = Object.values(checkedAmenities).join(', ');
-        $('div.amenities').text(amenitiesList)
+$(document).ready(function () {
+  const checkedAmenities = {};
+  function updateCheckboxes () {
+    $('input[type="checkbox"]').each(function () {
+      const amenId = $(this).attr('data-id');
+      if (amenId in checkedAmenities) {
+        $(this).prop('checked', true);
+      } else {
+        $(this).prop('checked', false);
+      }
     });
+  }
+  function updateAmenitiesList () {
+    const h4Content = Object.values(checkedAmenities).join(', ');
+    $('div.amenities h4').text(h4Content);
+  }
+
+  $('input[type="checkbox"]').click(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+    if ($(this).is(':checked')) {
+      checkedAmenities[amenityId] = amenityName;
+    } else {
+      delete checkedAmenities[amenityId];
+    }
+    updateCheckboxes();
+    updateAmenitiesList();
+  });
+  updateCheckboxes();
+  updateAmenitiesList();
 });
